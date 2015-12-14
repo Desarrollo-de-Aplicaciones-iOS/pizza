704bfe566dcc5f7a7d44dfa22e1f53af0295dae1
Luis Perez Romero <pinoluis02@gmail.com>
2015-12-13 22:07:37 -0600

    Initial Commit

---
+++ pizza/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pizza
+//
+//  Created by Luis Rufino Perez Romero on 12/13/15.
+//  Copyright © 2015 Luis Perez Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
